c6ecd8bcc0126d46eeb36a20410635547aa2fcf3
Zueva Natalia <natta_liya@vk.com>
2023-09-09 13:46:00 +0800

    first commit

---
+++ 09.09/1.py
@@ -0,0 +1,25 @@
+def calc(a, b, c):
+    try:
+        a, b, c = int(a), str(b), int(c)
+    except ValueError:
+        return 'Введено не число'
+    else:
+        match b:
+            case '+':
+                return a + c
+            case '-':
+                return a - c
+            case '*':
+                return a * c
+            case '/':
+                if c != 0:
+                    return a / c
+                else:
+                    return "Не стоит делить нa ноль"
+            case _:
+                return "Введен не правильный оператор"
+
+
+while True:
+    a, b, c = input('num-operator-num: ').split()
+    print(calc(a, b, c))
